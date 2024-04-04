0096e19e498df7566fdfda06e579064e98ee1eb9
Shukri Hussein <husseinshukri@outlook.com>
2024-04-04 13:49:59 -0400

    lab-conmplete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
+
 def write_file(file_name, file_content):
-    pass
+    # writes content to a .txt file
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    # appends content to a .txt file.
+    with open(f"{file_name}.txt", "a") as file:
+        file.write("\n" + append_content.lstrip())
 
 def read_file(file_name):
-    pass
+    # reads and returns the content of a .txt file.
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
